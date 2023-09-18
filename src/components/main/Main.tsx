8f5395499666507d916f6e0434512a8cbb85f4cf
Manmay <manmay.singh@gmail.com>
2023-09-19 01:11:17 +0530

    used svg as component

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -4,8 +4,13 @@ export default function Main() {
   return (
     <>
       <div className="flex flex-col justify-center items-center">
-        <input type="text" className="w-[50%] mb-10" placeholder="Enter the Task" />
-        <Task />
+        <input
+          type="text"
+          className="w-[50%] mb-10"
+          placeholder="Enter the Task"
+        />
+        <Task name="Groccery Shopping" description="Need to purchase the Groccerry from Smart Bazaar of boring road." />&nbsp;
+        <Task name="Groccery Shopping" description="Need to purchase the Groccerry from Smart Bazaar of boring road." />&nbsp;
       </div>
     </>
   );
--- src/components/main/Task.tsx
+++ src/components/main/Task.tsx
@@ -1,7 +1,30 @@
-export default function Task(){
-    return (
-        <>
-            <span className="text-white">Task Component.</span>
-        </>
-    )
+import Something from "../../assets/RightArrow";
+
+
+interface Task_interface {
+  name?: string;
+  description?: string;
+}
+
+export default function Task({ name, description }: Task_interface) {
+  return (
+    <>
+      <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+        <a>
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-lime-700">
+            {name}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {description}
+        </p>
+        <a className="inline-flex cursor-pointer items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <Something />
+        </a> &nbsp;
+        <a className="inline-flex cursor-pointer items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+          <Something />
+        </a>
+      </div>
+    </>
+  );
 }
